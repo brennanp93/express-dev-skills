47fe7dd7d770cf862be8a2a57e6133d6263f7cae
brennanp93 <brennan.p.perez@gmail.com>
2022-10-19 21:06:52 -0700

    Add Delete Button

--- routes/skills.js
+++ routes/skills.js
@@ -2,8 +2,23 @@ var express = require('express');
 var router = express.Router();
 var skillsController = require('../controllers/skills');
 
-/* GET users listing. */
-router.get('/', skillsController.index);
 
+/* GET users listing. */
+//GET /todos
+router.get('/', skillsController.index);
+// GET /todos/new <-- define BEFORE show route
+router.get('/new', skillsController.new);
+//GET /todos/:id
 router.get('/:id', skillsController.show);
+// GET/todos/:id/edit
+// POST /todos
+router.post('/', skillsController.create);
+// DELETE/todos/:id
+router.delete('/:id', skillsController.delete);
+// PUT/todos/:id
+
+
+
+
+
 module.exports = router;
